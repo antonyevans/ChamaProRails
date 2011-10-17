3da3b37b998925fae89cb38db110747c86d984bc
Antony Evans <antonyevans@gmail.com>
2011-10-17 17:40:09 +0100

    edited sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,16 @@
-class SessionsController < Devise::SessionsController
-  
+class Managers::SessionsController < Devise::SessionsController
   def create
-    resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
-    return sign_in_and_redirect(resource_name, resource)
-  end
-  
-  def sign_in_and_redirect(resource_or_scope, resource=nil)
-    scope = Devise::Mapping.find_scope!(resource_or_scope)
-    resource ||= resource_or_scope
-    sign_in(scope, resource) unless warden.user(scope) == resource
-    return render :json => {:success => true, :auth_token => current_user.authentication_token, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
-  end
+    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
 
-  def failure
-    return render:json => {:success => false, :errors => ["Login failed."]}
+    respond_to do |format|
+      format.html do
+        respond_with resource, :location => redirect_location(resource_name, resource)
+      end
+      format.json do
+        render :json => { :response => 'ok', :auth_token => current_user.authentication_token }.to_json, :status => :ok
+      end
+    end
   end
-end
+end 
